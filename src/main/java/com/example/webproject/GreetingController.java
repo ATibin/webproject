17055a14a72e557807d9c150f3e5357fe5dfb33f
ATibin <andrejtibin84@gmail.com>
2022-08-21 20:00:01 +0300

    JPA

--- src/main/java/com/example/webproject/GreetingController.java
+++ src/main/java/com/example/webproject/GreetingController.java
@@ -1,13 +1,21 @@
 package com.example.webproject;
 
+import com.example.webproject.domain.Message;
+import com.example.webproject.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false,
@@ -17,8 +25,31 @@ public class GreetingController {
     }
     @GetMapping
     public String main(Map<String, Object> model) {
-            model.put("some", "Yoo");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
             return "main";
+        }
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+       Message message = new Message(text, tag);
+       messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+        return "main";
+        }
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter,  Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
         }
     }
 
