5e5411cb527812bdd2f2e027a7fb757c5e1e1eda
ATibin <andrejtibin84@gmail.com>
2022-08-23 13:39:47 +0300

    init

--- src/main/java/com/example/webproject/controller/MainController.java
+++ src/main/java/com/example/webproject/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.webproject.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +22,20 @@ public class MainController {
     public String greeting(Map<String, Object> model) {
         return "greeting";
     }
-    @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
-            return "main";
+    @GetMapping("/main")
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        if( filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
         }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+        return "main";
+        }
+
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
@@ -35,22 +43,11 @@ public class MainController {
             @RequestParam String tag, Map<String, Object> model){
        Message message = new Message(text, tag, user);
        messageRepo.save(message);
-        Iterable<Message> messages = messageRepo.findAll();
+       Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
-        return "main";
+       model.put("messages", messages);
+       return "main";
         }
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter,  Map<String, Object> model) {
-        Iterable<Message> messages;
 
-        if(filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        }else{
-            messages = messageRepo.findAll();
-        }
-        model.put("messages", messages);
-        return "main";
-        }
     }
 
