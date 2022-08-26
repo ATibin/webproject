26808c1b8daf7b9fc9f499916f308b454e43b6d4
ATibin <andrejtibin84@gmail.com>
2022-08-26 16:15:02 +0300

    init

--- src/main/java/com/example/webproject/domain/User.java
+++ src/main/java/com/example/webproject/domain/User.java
@@ -16,6 +16,8 @@ public class User implements UserDetails {
     private String username;
     private String password;
     private boolean active;
+    private String email;
+    private String activationCode;
 
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
@@ -90,5 +92,21 @@ public class User implements UserDetails {
 
     public String getUsername() {
         return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getActivationCode() {
+        return activationCode;
+    }
+
+    public void setActivationCode(String activationCode) {
+        this.activationCode = activationCode;
     }
 }
--- src/main/java/com/example/webproject/repos/UserRepo.java
+++ src/main/java/com/example/webproject/repos/UserRepo.java
@@ -4,5 +4,9 @@ import com.example.webproject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
+
+    User findByActivationCode(String code);
+
 }
