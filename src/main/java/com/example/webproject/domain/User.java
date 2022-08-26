51008a35b2926124ce822057fc8064f0684b52f1
ATibin <andrejtibin84@gmail.com>
2022-08-26 10:49:19 +0300

    init

--- src/main/java/com/example/webproject/domain/User.java
+++ src/main/java/com/example/webproject/domain/User.java
@@ -17,11 +17,15 @@ public class User implements UserDetails {
     private String password;
     private boolean active;
 
+
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin() {
+        return roles.contains(Role.ADMIN);
+    }
 
     @Override
     public boolean isAccountNonExpired() {
